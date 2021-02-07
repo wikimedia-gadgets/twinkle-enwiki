6c7a2a4461cfdbb0227091941ab374ec2246ff46
Siddharth VP <siddharthvp@gmail.com>
2021-02-07 17:05:28 +0530

    add

---
+++ src/messages.ts
@@ -0,0 +1,42 @@
+
+const messages = {
+	'info': 'Info',
+	'note': 'Note',
+
+	// Tag
+	'status-added': 'Added $1 {{plural:$1|tag|tags}}',
+	'status-removed': 'Removed $1 {{plural:$1|tag|tags}}',
+	'status-added-removed': 'Added $1 {{plural:$1|tag|tags}}; Removed $2 {{plural:$2|tag|tags}}',
+	'untag-from-read': 'For removal of existing tags, please open Tag menu from the current version of article',
+	'tags-present-header': 'Tags already present',
+	'mark-patrolled': 'Mark the page as patrolled/reviewed',
+	'tag-complete-reloading': 'Tagging complete, reloading $1 in a few seconds',
+	'select-one': 'You must select at least one tag!',
+	'getting-redirects': 'Getting template redirects',
+	'cant-reposition': 'Failed to find {{$1}} on the page... skip repositioning',
+	'cant-remove': 'Failed to find {{$1}} on the page... cannot remove',
+	'untagging': 'Untagging',
+	'removing': 'Already present tags: removing de-selected tags',
+	'resolved-redirect': 'Resolved template redirect {{$1}} to {{$2}}',
+	'tagging-status': 'Tagging $1',
+
+	// Speedy
+
+
+	// XFD
+	'window-title': 'Start a deletion discussion (XfD)',
+	'default-tootip': 'Start a deletion discussion',
+	'discussion-venue-tooltip': 'When activated, a default choice is made, based on what namespace you are in. This default should be the most appropriate.',
+	'notify-checkbox-label': 'Notify page creator if possible',
+	'notify-checkbox-tooltip': "A notification template will be placed on the creator's talk page if this is true.",
+	'unrecognised-venue': 'Unrecognised venue: $1',
+	'reason-tooltip': 'You can use wikimarkup in your reason. Twinkle will automatically sign your post.',
+	'deletion-reason-here': 'Your deletion rationale is provided below, which you can copy and paste into a new XFD dialog if you wish to try again:',
+	'nomination-complete-redirect': "Nomination complete, now redirecting to the discussion page",
+	'looking-old-nominations': 'Looking for prior nominations of this page',
+
+
+}
+
+
+export default messages;
