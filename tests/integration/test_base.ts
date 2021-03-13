1faf02498543ee2d7e2f449627d7c1b87eb2ccef
Siddharth VP <siddharthvp@gmail.com>
2021-03-13 20:55:34 +0530

    test_base: use typescript

--- tests/integration/test_base.ts
+++ tests/integration/test_base.ts
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const {mwn} = require('mwn');
+export const fs = require('fs');
+export const { mwn } = require('mwn');
 
 // External API clients to make and observe changes
 let mwnConfig = {
 	apiUrl: 'http://localhost:8080/api.php',
-	password: '12345678901234567890123456789012' // BotPassword configured in setup.sh
-}
+	password: '12345678901234567890123456789012', // BotPassword configured in setup.sh
+};
 
-const bot = new mwn({ ...mwnConfig, username: 'Wikiuser@bp' });
-const bot2 = new mwn({ ...mwnConfig, username: 'Wikiuser2@bp' });
+export const bot = new mwn({ ...mwnConfig, username: 'Wikiuser@bp' });
+export const bot2 = new mwn({ ...mwnConfig, username: 'Wikiuser2@bp' });
 
-function setupMwn() {
+export function setupMwn() {
 	// Don't login again if already logged in (from another file)
 	if (!bot.loggedIn) {
 		// Login the 1st account. The 2nd account needs to sign in only if required
@@ -18,7 +18,7 @@ function setupMwn() {
 	}
 }
 
-async function setupMWBrowser(page) {
+export async function setupMWBrowser(page) {
 	await page.goto('http://localhost:8080/index.php?title=Special:UserLogin');
 	await page.fill('#wpName1', 'Wikiuser');
 	await page.fill('#wpPassword1', 'wikipassword');
@@ -28,7 +28,7 @@ async function setupMWBrowser(page) {
 	]);
 }
 
-async function loadExpectInBrowser() {
+export async function loadExpectInBrowser() {
 	// Also load expect in the browser context so that we can make expect assertions in browser context too
 	// However, note that these if these fail, no meaningful logging error messages may be displayed in the
 	// console. Due to this, minimise use of browser context assertions as far as possible.
@@ -41,7 +41,7 @@ async function readFile(path) {
 	return (await fs.promises.readFile(path)).toString();
 }
 
-async function loadTwinkle() {
+export async function loadTwinkle() {
 	const twinkleCore = __dirname + '/../../../twinkle-core/';
 	const repoRoot = __dirname + '/../../';
 
@@ -50,16 +50,15 @@ async function loadTwinkle() {
 	});
 	// we can do without the css peer gadget
 	await page.addStyleTag({
-		content: await readFile(twinkleCore + 'morebits/morebits.css') +
-			'\n' + await readFile(repoRoot + 'src/twinkle.css')
+		content:
+			(await readFile(twinkleCore + 'morebits/morebits.css')) + '\n' + (await readFile(repoRoot + 'src/twinkle.css')),
 	});
 	await page.evaluate(await readFile(twinkleCore + 'morebits/morebits.js'));
 	await page.evaluate(await readFile(repoRoot + 'build/bundle.js'));
 }
 
-
-class TwinkleModule {
-	static twinkleLoaded = false;
+export class TwinkleModule {
+	moduleName: string;
 	constructor(moduleName) {
 		this.moduleName = moduleName;
 	}
@@ -75,31 +74,39 @@ class TwinkleModule {
 		return this;
 	}
 	async submit() {
-		// XXX: Assumes only one quickForm is open at a time
+		// Assumes only one quickForm is open at a time
 		await page.click('.morebits-dialog-buttons button');
 		await page.waitForSelector('.morebits_action_complete');
 	}
 }
 
-
-async function createAndGotoRandomPage(prefix, content, summary) {
+export async function createRandomPage(prefix, content?, summary?) {
 	const pageName = prefix + '/' + rand();
 	await bot.create(pageName, content || 'Test page', summary);
+	return pageName;
+}
+
+export async function goto(pageName: string) {
+	if (pageName.startsWith('?')) {
+		await page.goto('http://localhost:8080/index.php' + pageName);
+	} else if (pageName.startsWith('/')) {
+		// path provided
+		await page.goto('http://localhost:8080/index.php/wiki' + pageName);
+	} else {
+		await page.goto('http://localhost:8080/index.php/' + mwn.util.wikiUrlencode(pageName));
+	}
+}
+
+export async function createAndGotoRandomPage(prefix, content?, summary?) {
+	const pageName = await createRandomPage(prefix, content, summary);
 	await goto(pageName);
 	return pageName;
 }
 
-async function goto(pageName) {
-	return await page.goto('http://localhost:8080/index.php/' + mwn.util.wikiUrlencode(pageName));
-}
-
-function rand(digits = 5) {
+export function rand(digits = 5) {
 	return String(Math.random()).slice(2, 2 + digits);
 }
 
-async function readText(pageName) {
+export async function readText(pageName) {
 	return (await bot.read(pageName)).revisions[0].content;
 }
-
-module.exports = { fs, mwn, bot, bot2, setupMwn, setupMWBrowser, loadExpectInBrowser,
-	createAndGotoRandomPage, goto, rand, readText, TwinkleModule, loadTwinkle };
