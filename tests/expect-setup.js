715f8eee262bf14888f8a50066c6cd10dd16a01d
Siddharth VP <siddharthvp@gmail.com>
2021-03-14 09:02:32 +0530

    add tests for prod

--- tests/expect-setup.js
+++ tests/expect-setup.js
@@ -13,22 +13,22 @@ chai.use(chaiAsPromised);
 // Make sure chai and jest ".not" play nice together
 const originalNot = Object.getOwnPropertyDescriptor(chai.Assertion.prototype, 'not').get;
 Object.defineProperty(chai.Assertion.prototype, 'not', {
-    get() {
-        Object.assign(this, this.assignedNot);
-        return originalNot.apply(this);
-    },
-    set(newNot) {
-        this.assignedNot = newNot;
-        return newNot;
-    },
+	get() {
+		Object.assign(this, this.assignedNot);
+		return originalNot.apply(this);
+	},
+	set(newNot) {
+		this.assignedNot = newNot;
+		return newNot;
+	},
 });
 
 // Combine both jest and chai matchers on expect
 const originalExpect = global.expect;
 
 global.expect = (actual) => {
-    const originalMatchers = originalExpect(actual);
-    const chaiMatchers = chai.expect(actual);
-    const combinedMatchers = Object.assign(chaiMatchers, originalMatchers);
-    return combinedMatchers;
+	const originalMatchers = originalExpect(actual);
+	const chaiMatchers = chai.expect(actual);
+	const combinedMatchers = Object.assign(chaiMatchers, originalMatchers);
+	return combinedMatchers;
 };
---
+++ tests/integration/prod.test.ts
@@ -0,0 +1,44 @@
+import { setupMWBrowser, setupMwn, bot, goto, TwinkleModule, readText, bot2 } from './test_base';
+
+describe('prod', () => {
+	jest.setTimeout(500000);
+
+	beforeAll(async () => {
+		await Promise.all([setupMwn(), setupMWBrowser(page), bot2.login()]);
+	});
+
+	it('prod', async () => {
+		// create with bot2 so self-notification won't be aborted
+		await bot2.create('Prod test/1', 'Prod test page');
+		await goto('Prod test/1');
+		let prod = await new TwinkleModule('prod').open();
+		await page.fill('.quickform textarea[name="reason"]', 'PROD CONCERN');
+		await prod.submit();
+
+		await expect(await readText('Prod test/1')).toContain('{{subst:prod|1=PROD CONCERN|help=off}}');
+		await expect(await readText('User talk:Wikiuser2')).toContain(
+			'{{subst:prod|1=PROD CONCERN|help=off}} [[User:Wikiuser' // require sig
+		);
+	});
+
+	it('blp prod', async () => {
+		await bot2.create('Prod blp test/1', 'Prod test page');
+		await goto('Prod blp test/1');
+		let prod = await new TwinkleModule('prod').open();
+		await page.click('.quickform input[value="prodblp"]');
+		await prod.submit();
+
+		await expect(await readText('Prod test/1')).toContain(
+			'{{subst:proposed deletion notify|1=Prod test/1|concern=PROD CONCERN}}'
+		);
+	});
+
+	afterAll(async () => {
+		await Promise.all([
+			bot.delete('Prod test/1'),
+			bot.delete('Talk:Prod test/1'),
+			bot.delete('Prod blp test/1'),
+			bot.delete('Talk:Prod blp test/1'),
+		]);
+	});
+});
