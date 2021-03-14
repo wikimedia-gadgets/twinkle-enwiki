22c20ba5c62dfb22d6d412e6d3629daa3a128eab
Siddharth VP <siddharthvp@gmail.com>
2021-03-14 19:26:44 +0530

    add i18n for batchundelete

--- src/batchundelete.ts
+++ src/batchundelete.ts
@@ -1,3 +1,8 @@
 import { BatchUndeleteCore } from './core';
 
-export class BatchUndelete extends BatchUndeleteCore {}
+export class BatchUndelete extends BatchUndeleteCore {
+	footerlinks = {
+		'Twinkle help': 'WP:TW/DOC#batchundelete',
+		'Give feedback': 'WT:TW',
+	};
+}
