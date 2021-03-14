cfde073464a06febb50255a5b9adedcf8dde376d
Siddharth VP <siddharthvp@gmail.com>
2021-03-14 13:34:15 +0530

    add arv to twinkle-enwiki; almost unmodified

--- src/twinkle.ts
+++ src/twinkle.ts
@@ -14,6 +14,7 @@ import { Deprod } from './deprod';
 import { Welcome } from './welcome';
 import { Shared } from './shared';
 import { Talkback } from './talkback';
+import { Arv } from './arv';
 
 // no customisation; import directly from core
 import { DiffCore as Diff } from './core';
@@ -50,6 +51,7 @@ Twinkle.registeredModules = [
 	Block,
 	Prod,
 	Deprod,
+	Arv,
 	Welcome,
 	Shared,
 	Talkback,
